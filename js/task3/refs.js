47bc2f08f48c3b98ad1e4867abedb01379d683ba
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-09-29 03:39:09 +0300

    cleaned folder

--- js/task3/refs.js
+++
@@ -1,5 +0,0 @@
-const imageList = document.querySelector(".gallery");
-const searchForm = document.querySelector("#search-form");
-// const btnSearch = document.querySelector('.btn-loadmore');
-
-export { imageList, searchForm };
